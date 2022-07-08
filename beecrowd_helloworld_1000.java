cef706d16b69dae0bfed76daa227c0975435ad6a
Brenda <brenda.leite1@hotmail.com>
2022-07-08 18:02:32 -0300

    Initial commit

---
+++ beecrowd_helloworld_1000.java
@@ -0,0 +1,30 @@
+
+/*
+ * Seu primeiro programa em qualquer linguagem de programação geralmente é
+ * " Hello World! ". Neste primeiro problema, basta imprimir esta mensagem na
+ * tela.
+ * 
+ * Entrada
+ * Este problema não tem entrada.
+ * 
+ * Resultado
+ * Você deve imprimir a mensagem Hello World! e, em seguida, a linha final como
+ * mostrado abaixo.
+ */
+import java.io.IOException;
+
+/**
+ * IMPORTANT:
+ * O nome da classe deve ser "Main" para que a sua solução execute
+ * Class name must be "Main" for your solution to execute
+ * El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class beecrowd_helloworld_1000 {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
